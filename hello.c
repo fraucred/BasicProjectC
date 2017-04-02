6ac73ee054c00ec6ac4bdca6766d24c8b96e802d
fraucred <ha_257@hotmail.com>
2017-04-02 15:23:44 +0200

    J'ai fini l'exercice 1

--- hello.c
+++ hello.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-main(int argc, char ** argc){
+int main(int argv, char ** argc) {
     char point = '.';
-    char str [81];
+    char str[81];
     char strSpace[81];
     int i;
     int j;
-    int cpt;
-    
+    int cpt = 0;
+
     //Saisir une phrase
     fflush(stdin);
     gets(str);
-    
-    //Parcourir le tableau
-    for(i = 0; i < str; i++) {
-        if(str[i] = '  '){ //Si le tableau contient deux espaces (car le premier espace n'est pas pris en compte)
-        cpt++;
-    }
-}
-    for (j = 0; j < cpt; j++) {
-        strSpace[j] = ' '
 
-}
-        for(j = cpt+1; j < 81 -cpt; j++) {
-            strSpace[j] = str[i];
+    //Parcourir le tableau
+    for (i = 0; i < 81; i++) {
+        if (str[i] == '  ') { //Si le tableau contient deux espaces (car le premier espace n'est pas pris en compte)
+            cpt++;
         }
+    }
+    for (j = 0; j < cpt; j++) {
+        strSpace[j] = ' ';
+
+    }
+    for (j = cpt + 1; j < 81 - cpt; j++) {
+        strSpace[j] = str[i];
+    }
+    return 0;
+}
